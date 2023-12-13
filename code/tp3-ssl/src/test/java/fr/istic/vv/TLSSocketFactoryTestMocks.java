8d024e980d9351e7aeac4073666b251a013172e0
clara <poncetcla@gmail.com>
2023-12-13 15:54:21 +0100

    removed unused imports

--- code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
+++ code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
@@ -3,11 +3,6 @@ package fr.istic.vv;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TLSSocketFactoryTestMocks {
